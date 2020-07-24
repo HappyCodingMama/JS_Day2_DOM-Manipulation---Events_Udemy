bb0994d656a0d36520ac192d4724e533c9ac051f
HappyCodingMama <HappyCodingMama@gmail.com>
2020-07-24 20:01:14 +0900

    studying about JS

---
+++ script.js
@@ -0,0 +1,70 @@
+// const cardTitle = document.getElementById('card-title')
+
+// cardTitle.innerHTML = '<h1 style="color:red">Title</h1>';
+
+
+// const changeCode = document.querySelector('ul li')
+
+
+// changeCode.style.color = 'orangered';
+// document.querySelector('li:last-child').style.color = 'pink';
+
+
+// document.querySelector('li:nth-child(2)').style.color = 'blue';
+
+// document.querySelector('li:nth-child(3)').innerText = 'write a diary';
+
+// document.querySelector('li:nth-child(odd)').style.backgroundColor = '#f4f4f4';
+
+
+// const item = document.getElementsByClassName('collection-item');
+
+// console.log(item);
+// console.log(item[0]);
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+
+// console.log(listItems);
+
+//Convert HTML collection into array
+// let lis = document.getElementsByTagName('li');
+
+// lis=Array.from(lis);
+
+// lis.reverse();
+    
+//  lis.forEach(function(li, index){
+//     console.log(li.className);
+//     li.innerText =`${index}: Hello`;
+//  });   
+ 
+// console.log(lis);
+
+
+// const items = document.querySelectorAll('li.collection-item');
+
+// //  items.forEach(function(item, index){
+// //     item.innerText =`${index}: Hello`;
+// //  });   
+
+// items.forEach(function(item){
+//     item.style.color = 'orangered';
+
+//  });
+
+ 
+//  const liOdd = document.querySelectorAll('li:nth-child(odd)');
+
+//  liOdd.forEach(function(li){
+//     li.style.backgroundColor = '#f3f3f3';
+//  });
+ 
+
+//  const liEven = document.querySelectorAll('li:nth-child(even)');
+
+//  liEven.forEach(function(li) {
+//      li.innerText = 'orangered';
+//  });
+
+// console.log(liOdd, liEven);
+
